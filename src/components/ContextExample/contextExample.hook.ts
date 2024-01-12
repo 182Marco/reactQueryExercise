0f60f3621492fdcb864cdbb3e138403420cf2185
Marco Milza <marco@MacBook-Pro-di-Marco.local>
2024-01-12 19:06:57 +0100

    minor: added comments

--- src/components/ContextExample/contextExample.hook.ts
+++ src/components/ContextExample/contextExample.hook.ts
@@ -1,4 +1,4 @@
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 import { useState } from 'react';
 import { wait, posts, singlePost } from '../utils';
 
@@ -8,8 +8,9 @@ export const useContextExample = () => {
   const newPostMutation = useMutation({
     mutationFn: (title: string) =>
       wait(3000).then(() => posts.push({ ...singlePost, title: title })),
-    onMutate: vars => ({ firstProp: 1 }),
+    onMutate: vars => ({ firstProp: 1 }), // perform actions before the mutation or set values inside the context
     onSuccess: (data, vars, context) => setMutationContext(context),
+    onSettled: () => {}, // similar to 'finally' in Promises
   });
 
   return {
