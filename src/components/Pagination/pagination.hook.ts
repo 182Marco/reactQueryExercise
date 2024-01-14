323151255ca80e6b36e9d2b4353ebc2c277ba2d3
Marco Milza <marco@MacBook-Pro-di-Marco.local>
2024-01-14 23:23:14 +0100

    feat: done pagination with also prefetching

--- src/components/Pagination/pagination.hook.ts
+++ src/components/Pagination/pagination.hook.ts
@@ -1,17 +1,28 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { paginatedJphPostsUrl } from '../../utils';
 
 export const usePagination = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const maxPage = 25;
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    const nextPage = currentPage + 1;
+    queryClient.prefetchQuery({
+      queryKey: ['posts', nextPage],
+      queryFn: async () => {
+        const res = await axios.get(paginatedJphPostsUrl(currentPage));
+        return res.data;
+      },
+    });
+  }, [currentPage, queryClient]);
 
   const postQuery = useQuery({
     queryKey: ['posts', currentPage],
     queryFn: async () => {
       const res = await axios.get(paginatedJphPostsUrl(currentPage));
-      console.log(`marcom ---> currentPage: `, currentPage);
       return res.data;
     },
   });
